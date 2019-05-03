1791ec166f2e22586de9bf6d7002fa7195e3bfc9
David Benavides <davidjose.benavides@gmail.com>
2019-05-03 16:31:54 -0500

    DB: Respaldo

--- src/app/pages/producto/producto.component.ts
+++ src/app/pages/producto/producto.component.ts
@@ -20,14 +20,13 @@ export class ProductoComponent implements OnInit {
 
   constructor( private activatedRoute: ActivatedRoute,
     public woo: WoocommerceService) {
-
-    this.activatedRoute.params
-      .subscribe( params => {
-        this.producto = params['id'];
-        console.log(this.producto);
-        this.woo.getProducto( this.producto );
-      });
-  }
+      this.activatedRoute.params
+        .subscribe( params => {
+          this.producto = params['id'];
+          console.log(this.producto);
+          this.woo.getProducto( this.producto );
+        });
+      }
 
   ngOnInit() {}
 
